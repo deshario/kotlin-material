9758b3d1591c420794c7d100670f626046162f24
deshario <deshario9@gmail.com>
2020-09-28 14:11:35 +0700

    Initial Bottom Navigation

---
+++ app/src/main/java/com/example/bottomnav/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottom_navigation, navController)
+
+        bottom_navigation.selectedItemId = R.id.aboutFragID
+    }
+
+}
---
+++ app/src/main/java/com/example/bottomnav/fragments/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.example.bottomnav.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bottomnav.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
